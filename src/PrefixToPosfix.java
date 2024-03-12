62db9433a8f03989df3e011299389a855c0ee640
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-03-11 19:37:41 -0600

    Logica para convertir expresion prefix a postfix

--- src/PrefixToPosfix.java
+++ src/PrefixToPosfix.java
@@ -36,4 +36,46 @@ public class PrefixToPosfix
         expresion = temp;
         return expresion;
     }
+
+    /** 
+     * @param expresion
+     * @return String
+     */
+    public static String convPrefixToPosfix(String expresion){
+        expresion = invertir(expresion);
+        String temp = "";
+        StackArrayList<String> stack = new StackArrayList<>();
+        for(int i = 0; i<expresion.length(); i++){
+            if(expresion.charAt(i) >= 'a' && expresion.charAt(i) <= 'z'){
+                stack.push(""+expresion.charAt(i));
+            } else if(expresion.charAt(i) == ' '){
+                stack.push(" ");
+            }else if (expresion.charAt(i) == '*' || expresion.charAt(i) == '+' || expresion.charAt(i) == '-' || expresion.charAt(i) == '/'){
+                for(int a = 0; a<= 1; a++){
+                    if(stack.peek() == " "){
+                        temp = temp+stack.pull();
+                    }
+                    if(!stack.isEmpty()){
+                    temp = temp+stack.pull();}
+                }
+                temp = temp+" "+expresion.charAt(i);
+                temp = temp.trim();
+            } else if (expresion.charAt(i) >= '0' && expresion.charAt(i) <= '9'){
+                String temp2 = "";
+                for(int a = i; a<expresion.length(); a++){
+                    if(expresion.charAt(a) >= '0' && expresion.charAt(a) <= '9'){
+                        temp2 = temp2+expresion.charAt(a);
+                        i = a;
+                    } else{
+                        stack.push(invertir(temp2));
+                        break;   
+                    }
+                }
+            } else if(expresion.charAt(i) == '(' || expresion.charAt(i) == ')'){
+                temp = temp+""+expresion.charAt(i);
+            }
+        }
+        temp = temp.replaceAll("  ", " ");
+        return temp;
+    }
 }
